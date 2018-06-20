60493e2f79a60070e536c311c7a6e332d0c5b2ba
DrAlexOne <DrAlexOne@users.noreply.github.com>
2018-06-20 21:27:07 +0300

    Added App

---
+++ UWP Clicker/UWP Clicker/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.ViewManagement;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace UWP_Clicker
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ForegroundColor = Windows.UI.Colors.Black;
+            titleBar.BackgroundColor = Windows.UI.Colors.WhiteSmoke;
+            titleBar.ButtonForegroundColor = Windows.UI.Colors.Black;
+            titleBar.ButtonBackgroundColor = Windows.UI.Colors.WhiteSmoke;
+        }
+        int clicks = 0;
+        int cn = 1;
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ItemsControl_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            clicks = clicks + cn;
+            txtBlock.Text = clicks.ToString();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (clicks>50)
+            {
+                clicks = clicks - 50;
+                cn = cn + 1;
+                txtBlock.Text = clicks.ToString();
+
+                ToastContent content = new ToastContent()
+                {
+
+                    Visual = new ToastVisual()
+                    {
+                        BindingGeneric = new ToastBindingGeneric()
+                        {
+                            Children =
+                            {
+                                new AdaptiveText()
+                                {
+                                    Text = "You bought booster!",
+                                    HintMaxLines = 1
+                                },
+                            }
+                        }
+                    },
+                };
+
+                var toast = new ToastNotification(content.GetXml());
+                ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+            }
+
+            else
+            {
+                
+                ContentDialog SClicks = new ContentDialog
+                {
+                Title = "Collect more  clicks!",
+                Content = "Collect 50 clicks and try again.",
+                CloseButtonText = "OK"
+                };
+
+                ContentDialogResult result = await SClicks.ShowAsync();
+                
+            }
+        }
+    }
+}
